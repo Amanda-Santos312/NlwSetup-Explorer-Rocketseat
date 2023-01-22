413204025d959ad2b9f9681d14c5d8374edd4f2a
Amanda <catce.2022111tads0053@aluno.ifpi.edu.br>
2023-01-21 21:22:05 -0300

    Updated

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+/* document -> objeto que existe em todo navegador
+.querySelector('a') -> seleciona a tag.*/
+const form = document.querySelector("#form-habits")
+const nlwSetup = new NLWSetup(form)
 
+const data = {
+  run: ["01-01","01-03","01-05"],
+  read: ["01-01", "01-02", "01-04"],
+  shop: ["01-03", "01-10"],
+  game: ["01-04", "01-08"],
+  movie: ["01-02", "01-10"]
+}
+nlwSetup.setData(data)
+nlwSetup.load()
