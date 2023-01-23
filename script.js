e3fc015baca06a5150e523ea80938503212c8563
Amanda <catce.2022111tads0053@aluno.ifpi.edu.br>
2023-01-22 22:11:28 -0300

    Updated

--- script.js
+++ script.js
@@ -2,7 +2,28 @@
 .querySelector('a') -> seleciona a tag.*/
 const form = document.querySelector("#form-habits")
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector('header button')
 
+button.addEventListener('click', add) //A ação de clicar vai adicionar 
+form.addEventListener("change", save)
+
+function add() {
+  const today = new Date().toLocaleDateString('pt-br').slice(0, -5)
+  const dayExists = nlwSetup.dayExists(today)
+
+  if(dayExists){
+    alert("Dia já incluso")
+    return
+  }
+
+  alert('Adicionado com sucesso')
+  nlwSetup.addDay(today)
+}
+
+function save(){
+console.log(nlwSetup.data)
+}
+/* 
 const data = {
   run: ["01-01","01-03","01-05"],
   read: ["01-01", "01-02", "01-04"],
@@ -12,3 +33,4 @@ const data = {
 }
 nlwSetup.setData(data)
 nlwSetup.load()
+*/
